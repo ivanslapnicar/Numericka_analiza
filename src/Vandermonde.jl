d04fe7392e86d70b08ed1aaa474172ce680900c9
Ivan Slapničar <ivan.slapnicar@fesb.hr>
2018-11-05 14:31:10 +0100

    changes

---
+++ src/Vandermonde.jl
@@ -0,0 +1,20 @@
+import Base.getindex, Base.size
+type Vandermonde{T} <: AbstractMatrix{T}
+	c :: Vector{T}
+end
+
+getindex(V::Vandermonde, i::Int, j::Int) = V.c[i]^(j-1)
+isassigned(V::Vandermonde, i::Int, j::Int) = isassigned(V.c, i)
+
+size(V::Vandermonde, r::Int) = (r==1 || r==2) ? length(V.c) :
+    throw(ArgumentError("Invalid dimension $r"))
+size(V::Vandermonde) = length(V.c), length(V.c)
+
+function full{T}(V::Vandermonde{T})
+	n=size(V, 1)
+	M=Array{T}(n, n)
+	for i=1:n
+		M[:,i] = V.c.^(i-1)
+	end
+	M
+end
