4140a43246c3009cf2bd3feac6bffae775c7de64
ivanslapnicar <ivan.slapnicar@fesb.hr>
2019-10-29 11:29:38 +0100

    Julia 1.1

--- src/Vandermonde.jl
+++ src/Vandermonde.jl
@@ -1,5 +1,5 @@
 import Base.getindex, Base.size
-type Vandermonde{T} <: AbstractMatrix{T}
+struct Vandermonde{T} <: AbstractMatrix{T}
 	c :: Vector{T}
 end
 
@@ -10,9 +10,9 @@ size(V::Vandermonde, r::Int) = (r==1 || r==2) ? length(V.c) :
     throw(ArgumentError("Invalid dimension $r"))
 size(V::Vandermonde) = length(V.c), length(V.c)
 
-function full{T}(V::Vandermonde{T})
+function Matrix(V::Vandermonde{T}) where T
 	n=size(V, 1)
-	M=Array{T}(n, n)
+	M=Array{T}(undef,n, n)
 	for i=1:n
 		M[:,i] = V.c.^(i-1)
 	end
