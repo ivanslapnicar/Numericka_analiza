3848edefe5fe40201762d74fc06889dc87f18e62
Ivan Slapnicar <ivan.slapnicar@fesb.hr>
2020-11-12 22:14:17 +0100

    Changes

---
+++ src/NA16 Primjene QR rastava.jl
@@ -0,0 +1,242 @@
+### A Pluto.jl notebook ###
+# v0.12.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 881ed68b-ac65-434a-9d62-df963fb033b1
+begin
+	using LinearAlgebra
+	using Plots
+	import Random
+	Random.seed!(123)
+end
+
+# ╔═╡ e0f03ec0-f136-411f-a270-bcc85fde0579
+md"""
+# Primjene QR rastava
+
+
+Ortogonalne matrice imaju dva važna svojstva:
+
+$Q^{-1}=Q^T$
+
+$\|Qx\|_2=\|x\|_2,\quad \forall x$
+
+Prvo svojstvo slijedi iz definicije ortogonalne matrice, jer je $Q^TQ=I$, 
+a drugo svojstvo slijedi iz
+
+$$\|Qx\|_2^2=(Qx)^T(Qx)=x^TQ^TQx=x^Tx=\|x\|_2^2.$$
+
+## Sustav linearnih jednadžbi
+
+QR rastav možemo koristiti za rješavanje sustava linearnih jednadžbi $Ax=b$:
+množenjem jednakosti $QRx=b$ s lijeva s $Q^T$ dobijemo $Rx=Q^Tb$ pa preostaje riješiti trokutasti sustav. 
+
+U odnosu na rješenje pomoću Gaussove eliminacije vrijedi:
+
+* broj računskih operacija se udvostruči,
+* rješenje je nešto točnije, i
+* nema rasta elemenata (pivotiranje nije potrebno).
+"""
+
+# ╔═╡ d0aa1de2-f2e5-4c59-b7ce-a0022bf6dbef
+begin
+	n=10
+	A=rand(n,n)
+	b=rand(n)
+	Q,R=qr(A)
+	c=transpose(Q)*b
+	# Trokutasti sustav
+	x=R\c
+end
+
+# ╔═╡ fbfa49ba-8c53-4fdf-ae26-d9b678fcce2e
+# Rezidual
+A*x-b
+
+# ╔═╡ 45923270-4d63-4eaf-8d99-f9e9baab558e
+md"""
+## Problem najmanjih kvadrata
+
+Programi za rješavanje problema najmanjih kvadrata uglavnom koriste QR rastav. Vrijedi
+
+$$
+\|Ax-b\|^2_2=\|QRx-b\|_2^2=\|Q(Rx-Q^Tb)\|_2=\|Rx-Q^Tb\|_2^2.$$
+
+Neka je
+
+$$
+R=\begin{bmatrix}R_0 \\ 0\end{bmatrix},\quad Q^Tb =\begin{bmatrix}c\\ d \end{bmatrix}.$$
+
+Tada je
+
+$$
+\|Rx-Q^T b\|_2^2 = \| R_0x-c\|_2^2+\|d\|_2^2$$
+
+pa je rješenje trokutastog sustava
+
+$$
+R_0x=c$$
+    
+rješenje problema najmanjih kvadrata. Riješimo zadatak iz bilježnice o regresiji:
+"""
+
+# ╔═╡ 2e4f479e-9a3a-4495-abb9-52a3203a35c4
+begin
+	y₁=[1,3,2,4,3]
+	A₁=transpose([1 2 3 6 7;1 1 1 1 1])
+end
+
+# ╔═╡ 743b75c7-d6a0-4fa0-bd99-6412378453e2
+#?qr  # Pogledajmo strukturu rješenja
+
+# ╔═╡ 09427c50-245e-11eb-0ee8-d54d6613381e
+Q₁
+
+# ╔═╡ 04bb12df-5d6a-460f-a381-23058dcfe0b7
+begin
+	F₁=qr(A₁)
+	c₁=transpose(Matrix(F₁.Q))*y₁
+	x₁=F₁.R\c₁
+end
+
+# ╔═╡ c4762bb2-56fd-4e2a-9255-4922f41addfe
+# Ugrađena funkcija
+A₁\y₁
+
+# ╔═╡ 881daa94-e922-4b76-bca3-6b255049ba67
+begin
+	# Veći primjer
+	m₂=8
+	n₂=5
+	A₂=rand(m₂,n₂)
+	b₂=rand(m₂)
+	F₂=qr(A₂)
+end
+
+# ╔═╡ 16461985-6cb3-4ddb-9eab-bd13917b8b69
+F₂.Q
+
+# ╔═╡ d01748e8-169d-4d39-bf79-90c0c0d2df56
+# Spremanje generatora
+F₂.factors
+
+# ╔═╡ ab0d603e-4e1f-4ec9-a247-c9c78c9e6488
+begin
+	# Rješenje
+	c₂=transpose(Matrix(F₂.Q))*b₂
+	x₂=F₂.R\c₂
+end
+
+# ╔═╡ f97a14a2-1e13-4a2d-81a6-c848666dbbfe
+# Ugrađena funkcija
+A₂\b₂
+
+# ╔═╡ 0ad1e3dc-e5d4-416a-b3e0-21fd710ed241
+md"""
+## Numerička  "ortogonalizacija" polinoma
+
+Numerička ortogonalizacija potencija vektora daje ortogonalne polinome.
+"""
+
+# ╔═╡ 24ac4e43-b99d-439e-aa6b-ba3bc494b8dd
+begin
+	# Standardna baza
+	xₒ=range(-1,stop=1,length=101)
+	# Kvazi Vandermondeova matrica
+	V=[xₒ.^0 xₒ.^1 xₒ.^2 xₒ.^3 xₒ.^4 xₒ.^5]
+end
+
+# ╔═╡ 9006213a-325f-4296-a0ec-d5c02b4ec5e8
+plot(xₒ,V,title="Standardna baza",legend=:bottomright)
+
+# ╔═╡ f157bb76-8445-4994-b08e-260b2a51ad51
+begin
+	# Ortogonalizacija s težinskom funkcijom ω=1 daje normirane Legendreove polinome.
+	Fₒ=qr(V)
+	Qₒ=Matrix(Fₒ.Q)*sign.(Diagonal(Fₒ.R))
+end
+
+# ╔═╡ 68d56539-22ab-4a89-ad51-fb3c2f37ee8d
+plot(xₒ,Qₒ,title="Legendreovi polinomi")
+
+# ╔═╡ 4b343213-f8d2-45d3-bae8-999dee675089
+md"""
+Dobiveni vektori su vrijednosti normiranih Legendreovih polinoma iz blježnice [NA12 Ortogonalni polinomi.ipynb](NA12%20Ortogonalni%20polinomi.ipynb).
+
+Da bi dobili Čebiševljeve polinome, trebamo dodati težinsku funkciju $\omega$ i preraditi funkciju `myGramSchmidtQR()` iz bilježnice [NA15 QR rastav.ipynb](NA15%20QR%20rastav.ipynb) tako da računa _težinske skalarne produkte_. Dobiveni vektori su vrijednosti normiranih Čebiševljevih polinoma.
+"""
+
+# ╔═╡ 830acec9-74f1-4b02-a5f7-b65b05f457ef
+function WeightedGramSchmidtQR(A::Array,ω::Vector)
+    m,n=size(A)
+    R=zeros(n,n)
+    Q=Array{Float64,2}(undef,m,n)
+    R[1,1]=norm(A[:,1])
+    Q[:,1]=A[:,1]/R[1,1]
+    for k=2:n
+        for i=1:k-1
+            R[i,k]=Q[:,i]⋅(A[:,k].*ω)/(Q[:,i]⋅(Q[:,i].*ω))
+        end
+        t=A[:,k]-sum([R[i,k]*Q[:,i] for i=1:k-1])
+        R[k,k]=norm(t)
+        Q[:,k]=t/R[k,k]
+    end
+    return Q,R
+end
+
+# ╔═╡ e0bf68c5-1e30-4621-871b-47eb71bfd2bb
+begin
+	x₃=range(-0.99,stop=0.99,length=101)
+	ω=1 ./(sqrt.(1.0.-x₃.^2))
+	# Kvazi Vandermonde-ova matrica
+	V₃=[x₃.^0 x₃.^1 x₃.^2 x₃.^3 x₃.^4 x₃.^5]
+end
+
+# ╔═╡ be2197e5-565e-4b13-acb9-a245f2f3780e
+begin
+	Q₃,R₃=WeightedGramSchmidtQR(V,ω)
+	Q₃=Q₃*sign.(Diagonal(R₃))
+end
+
+# ╔═╡ 957a675e-a12a-47bf-b196-bd4f3431849a
+plot(x₃,Q₃,title="Čebiševljevi polinomi")
+
+# ╔═╡ 656aa3d6-0ebc-4ca5-95d5-071af0f5c2c0
+md"""
+__Zadatak.__ Normirajte stupce matrice $Q₃$ tako da polinomi 
+poprimaju sve vrijednosti u intervalu $[-1,1]$. 
+"""
+
+# ╔═╡ 251721ca-a0c8-4346-ac31-fed3b1deff5d
+
+
+# ╔═╡ Cell order:
+# ╟─e0f03ec0-f136-411f-a270-bcc85fde0579
+# ╠═881ed68b-ac65-434a-9d62-df963fb033b1
+# ╠═d0aa1de2-f2e5-4c59-b7ce-a0022bf6dbef
+# ╠═fbfa49ba-8c53-4fdf-ae26-d9b678fcce2e
+# ╟─45923270-4d63-4eaf-8d99-f9e9baab558e
+# ╠═2e4f479e-9a3a-4495-abb9-52a3203a35c4
+# ╠═743b75c7-d6a0-4fa0-bd99-6412378453e2
+# ╠═09427c50-245e-11eb-0ee8-d54d6613381e
+# ╠═04bb12df-5d6a-460f-a381-23058dcfe0b7
+# ╠═c4762bb2-56fd-4e2a-9255-4922f41addfe
+# ╠═881daa94-e922-4b76-bca3-6b255049ba67
+# ╠═16461985-6cb3-4ddb-9eab-bd13917b8b69
+# ╠═d01748e8-169d-4d39-bf79-90c0c0d2df56
+# ╠═ab0d603e-4e1f-4ec9-a247-c9c78c9e6488
+# ╠═f97a14a2-1e13-4a2d-81a6-c848666dbbfe
+# ╟─0ad1e3dc-e5d4-416a-b3e0-21fd710ed241
+# ╠═24ac4e43-b99d-439e-aa6b-ba3bc494b8dd
+# ╠═9006213a-325f-4296-a0ec-d5c02b4ec5e8
+# ╠═f157bb76-8445-4994-b08e-260b2a51ad51
+# ╠═68d56539-22ab-4a89-ad51-fb3c2f37ee8d
+# ╟─4b343213-f8d2-45d3-bae8-999dee675089
+# ╠═830acec9-74f1-4b02-a5f7-b65b05f457ef
+# ╠═e0bf68c5-1e30-4621-871b-47eb71bfd2bb
+# ╠═be2197e5-565e-4b13-acb9-a245f2f3780e
+# ╠═957a675e-a12a-47bf-b196-bd4f3431849a
+# ╟─656aa3d6-0ebc-4ca5-95d5-071af0f5c2c0
+# ╠═251721ca-a0c8-4346-ac31-fed3b1deff5d
---
+++ src/NA17 Kompresija podataka.jl
@@ -0,0 +1,116 @@
+### A Pluto.jl notebook ###
+# v0.12.9
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
+# ╔═╡ 1dd2098b-8d4d-4fa4-a794-49badf9790c2
+begin
+	using Images
+	using LinearAlgebra
+	using Plots
+end
+
+# ╔═╡ c94e89d0-1049-4b0d-aa26-d5a1eda436b9
+md"""
+# Kompresija podataka
+
+QR rastav s pivotiranjem stupaca možemo koristiti za __kompresiju (sažimanje) podataka__.
+
+Dijagonalni elementi matrice $R$ padaju po apsolutnoj vrijednosti pa možemo odrezati djelove matrica $Q$ i $R$ za koje smatramo da nisu značajni.
+
+Dat ćemo primjer kompresije slike.
+"""
+
+# ╔═╡ 3d91d5e7-e134-464a-a0a4-032a28254715
+img=load("P8040001a.jpg")
+
+# ╔═╡ 464d999f-f530-4cc0-b9c8-fa094d8a138c
+# Opis podataka
+typeof(img)
+
+# ╔═╡ 86818c56-6d28-463a-aa87-3c0a715ef8ce
+img[1,1]
+
+# ╔═╡ ce428ce6-9bc5-4ce2-bb17-3bb8173fc937
+show(img[1,1])
+
+# ╔═╡ 1dfeac3f-51df-4fcf-919c-1675ae331189
+# Razdvojimo sliku na R, G i B komponente
+channels=channelview(img)
+
+# ╔═╡ c3ae4d9d-aa93-4ad8-af1d-fad775d9c9bd
+begin
+	Red=channels[1,:,:]
+	Green=channels[2,:,:]
+	Blue=channels[3,:,:]
+end
+
+# ╔═╡ c1cbc1a9-0a1e-4ac2-90fb-e9ce3c20be62
+colorview(Gray,Blue)
+
+# ╔═╡ c0cd1d7e-1fb9-4ae8-aa8d-794c36b85837
+begin
+	# Izračunajmo QR rastav s pivotiranjem matrice svakog kanala
+	R=qr(Red,Val(true))
+	G=qr(Green,Val(true))
+	B=qr(Blue,Val(true));
+end
+
+# ╔═╡ 32420d34-f066-4fdc-a488-15c4482640d8
+norm(R.Q*R.R[:,invperm(R.p)]-float(Red))
+
+# ╔═╡ c16bd7ca-a403-4f1b-adba-f0e9a028b14b
+# Nacrtajmo dijagonalne elemente
+scatter(1:length(diag(R.R,1)),abs.(diag(R.R)),
+    title="Diagonal elements of matrix R",legend=false)
+
+# ╔═╡ c26c9e80-2464-11eb-2866-9153dc112b78
+@bind k  html"<input type='range' min='10' max='200'"
+
+# ╔═╡ feebc2d6-e495-4145-8e90-3b0d7d073d04
+begin
+	# Izračunajmo komprimirane matrice za svaki kanal, RedC, GreenC i BlueC
+	# Funkcija Matrix() je nužna radi bržeg generiranja matrice Q
+	RedC=Matrix(R.Q)[:,1:k]*R.R[1:k,invperm(R.p)]
+	GreenC=Matrix(G.Q)[:,1:k]*G.R[1:k,invperm(G.p)]
+	BlueC=Matrix(B.Q)[:,1:k]*B.R[1:k,invperm(B.p)]
+end
+
+# ╔═╡ 955201fd-cb90-4c0f-bf50-ac1ca1850d26
+# Nacrtajmo komprimiranu sliku
+colorview(RGB, RedC, GreenC, BlueC)
+
+# ╔═╡ 5bb5fe50-2466-11eb-175c-e1ff94b7840c
+k
+
+# ╔═╡ b1c18ee7-4b8f-4b44-a960-d759040a29cd
+k, norm(Red-RedC)/norm(Red)
+
+# ╔═╡ Cell order:
+# ╟─c94e89d0-1049-4b0d-aa26-d5a1eda436b9
+# ╠═1dd2098b-8d4d-4fa4-a794-49badf9790c2
+# ╠═3d91d5e7-e134-464a-a0a4-032a28254715
+# ╠═464d999f-f530-4cc0-b9c8-fa094d8a138c
+# ╠═86818c56-6d28-463a-aa87-3c0a715ef8ce
+# ╠═ce428ce6-9bc5-4ce2-bb17-3bb8173fc937
+# ╠═1dfeac3f-51df-4fcf-919c-1675ae331189
+# ╠═c3ae4d9d-aa93-4ad8-af1d-fad775d9c9bd
+# ╠═c1cbc1a9-0a1e-4ac2-90fb-e9ce3c20be62
+# ╠═c0cd1d7e-1fb9-4ae8-aa8d-794c36b85837
+# ╠═32420d34-f066-4fdc-a488-15c4482640d8
+# ╠═c16bd7ca-a403-4f1b-adba-f0e9a028b14b
+# ╠═feebc2d6-e495-4145-8e90-3b0d7d073d04
+# ╠═5bb5fe50-2466-11eb-175c-e1ff94b7840c
+# ╠═955201fd-cb90-4c0f-bf50-ac1ca1850d26
+# ╠═c26c9e80-2464-11eb-2866-9153dc112b78
+# ╠═b1c18ee7-4b8f-4b44-a960-d759040a29cd
